086eef19e4ff206b461c37b59f39220bc08f9b25
Robert Romore <Robert.Romore@meredith.com>
2017-03-19 11:46:23 -0500

    Making adjustments to support actions on multiple gateways with one request instead of several

--- js/springboard_developer.js
+++ js/springboard_developer.js
@@ -19,30 +19,117 @@
           $select_actions.toggleClass('disabled', !($checkboxes.filter(':checked').length > 0));
         });
 
-        $checkboxes.on('change', function() {
-          $select_actions.toggleClass('disabled', !($checkboxes.filter(':checked').length > 0));
+        var refresh_actions = function() {
+          // Only enable the "disable all" button if at least one checked
+          // payment method's has a disable button in its row that is not
+          // disabled.
+          $select_actions.filter('.disable').toggleClass('disabled', !($checkboxes.filter(function(i, el) {
+            return $(el).is(':checked') && $(el).parents('tr').find('input[id^="disable-"]:enabled').length;
+          }).length));
+          // Only enable the "enable all" button if at least one checked
+          // payment method has an enable button in its row.
+          $select_actions.filter('.enable').toggleClass('disabled', !($checkboxes.filter(function(i, el) {
+            return $(el).is(':checked') && $(el).parents('tr').find('input[id^="enable-"]').length;
+          }).length));
+          // Always enable the other action buttons when any payment method is
+          // selected.
+          $select_actions.filter('.autoconfig, .autofill:visible').toggleClass('disabled', !($checkboxes.filter(':checked').length > 0));
           $all_checkbox.prop('checked', $checkboxes.length === $checkboxes.filter(':checked').length);
-        });
+        };
+
+        $checkboxes.on('change', refresh_actions);
+        $(document).on('ajaxComplete', refresh_actions);
 
         var $parent_tr;
-        $select_actions.click(function() {
+        $(document).on('click', '.select-actions input', function() {
           var $this = $(this);
-          $checkboxes.filter(':checked').each(function(index, value) {
-            var $value = $(value);
-            $parent_tr = $value.parents('tr');
-            if ($this.hasClass('disable')) {
-              $parent_tr.find('input[id^="disable-"]:visible').trigger('mousedown');
-            }
-            else if ($this.hasClass('enable')) {
-              $parent_tr.find('input[id^="enable-"]:visible').trigger('mousedown');
-            }
-            else if ($this.hasClass('autofill')) {
-              $parent_tr.find('input.lp-autofill-settings').trigger('click');
-            }
-            else if ($this.hasClass('autoconfig')) {
-              $parent_tr.find('input.auto-config-settings').trigger('click');
-            }
+          if ($this.hasClass('disabled')) {
+            return;
+          }
+
+          var action = '';
+          var class_name = '';
+          if ($this.hasClass('disable')) {
+            action = 0;
+            class_name = 'disable-payment-method';
+          }
+          else if ($this.hasClass('enable')) {
+            action = 1;
+            class_name = 'enable-payment-method';
+          }
+          else if ($this.hasClass('autofill')) {
+            action = 2;
+            class_name = '.lp-autofill-settings';
+          }
+          else if ($this.hasClass('autoconfig')) {
+            action = 3;
+            class_name = 'auto-config-settings';
+          }
+
+          var instance_ids = [];
+          var checked = $checkboxes.filter(':checked');
+          var buttons_to_modify = [];
+          checked.each(function(index, value) {
+            $parent_tr = $(value).parents('tr');
+            instance_ids.push($parent_tr.data('payment-method-id'));
+            var $b = $parent_tr.find('input.' + class_name);
+            buttons_to_modify.push($b);
+            $b.addClass('disabled').addClass('progress-disabled').prop('disabled', true);
           });
+          var $buttons_to_modify = $(buttons_to_modify);
+          instance_ids = instance_ids.join(',');
+
+          switch (action) {
+            case 0:
+            case 1:
+              var able = action == 0 ? 'disable' : 'enable';
+              $.get($this.data('url') + '/' + instance_ids + '/' + able, function(response) {
+                var buttons = response.split(',');
+                checked.each(function(index, value) {
+                  $parent_tr = $(value).parents('tr');
+                  $parent_tr.find('.' + able + '-payment-method').replaceWith(buttons[index]);
+                });
+              });
+              break;
+
+            case 2:
+            case 3:
+              var success_text = action == 2 ? 'Autofilled' : 'Autoconfigged';
+              var og_text = action == 2 ? 'LP Autofill' : 'Dev Config';
+              $.get($this.data('url') + '/' + instance_ids, function(response) {
+                if (response.length > 0) {
+                  response = $.parseJSON(response);
+                  alert(response.message);
+                  console.error(response.message);
+                  $buttons_to_modify.each(function(i, v) {
+                    $(v).val('Error').addClass('warning');
+                  });
+                  setTimeout(function() {
+                    $buttons_to_modify.each(function(i, v) {
+                      $(v).val(og_text).removeClass('warning');
+                    });
+                  }, 3000);
+                }
+                else {
+                  $buttons_to_modify.each(function(i, v) {
+                    var $v = $(v);
+                    var $next_tr = $v.parents('tr').next('tr.payment-method-settings-row');
+                    if ($next_tr.length > 0) {
+                      var $button = $v.siblings('.edit-payment-method-settings');
+                      $next_tr.remove();
+                      $button.click();
+                    }
+                    $v.val(success_text).addClass('success');
+                    setTimeout(function() {
+                      $v.val(og_text).removeClass('success');
+                    }, 3000);
+                  });
+                }
+                $buttons_to_modify.each(function(i, v) {
+                  $(v).prop('disabled', false).removeClass('progress-disabled');
+                });
+              });
+          }
         });
       });
 
@@ -155,6 +242,17 @@
         }
       });
 
+      // When a toggle button is clicked, make the request.
+      $toggle_button = $('.enable-payment-method, .disable-payment-method');
+      $(document).on('click', '.enable-payment-method, .disable-payment-method', function() {
+        var $this = $(this);
+        $this.prop('disabled', true).addClass('progress-disabled');
+        $.get($this.data('url'), function(response) {
+          $this.replaceWith(response);
+        });
+        return false;
+      });
+
       // When an autofill button is clicked, make the request.
       $lp_autofill = $('.lp-autofill-settings');
       $lp_autofill.on('click', function() {
@@ -188,8 +286,8 @@
       });
 
       // When an autoconfig button is clicked, make the request.
-      $lp_autoconfig = $('.auto-config-settings');
-      $lp_autoconfig.on('click', function() {
+      $autoconfig = $('.auto-config-settings');
+      $autoconfig.on('click', function() {
         var $this = $(this);
         $this.prop('disabled', true).addClass('progress-disabled');
         $.get($this.data('url'), function(response) {
@@ -209,7 +307,7 @@
               $next_tr.remove();
               $button.click();
             }
-            $this.val('Autoconfiged').addClass('success');
+            $this.val('Autoconfigged').addClass('success');
             setTimeout(function() {
               $this.val('Dev Config').removeClass('success');
             }, 3000);
